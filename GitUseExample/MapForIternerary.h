2de529406588ac74c4f7deb53c7b46800c0d9502
Ho Jimmy <y03131003@Jimmy.local>
2013-06-12 16:31:34 +0800

    add file

---
+++ GitUseExample/MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  GitUseExample
+//
+//  Created by Ho Jimmy on 13/6/12.
+//  Copyright (c) 2013年 Ho Jimmy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+@end
